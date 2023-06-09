9f5146538f78a94c2ec355f1327472ef73ad28d7
WaberMau <Andreas-jonss@outlook.com>
2023-06-09 16:14:21 +0200

    Client and SeatManager skeleton

--- Client.cs
+++ Client.cs
@@ -8,5 +8,20 @@ namespace Assignment2_Part2
 {
     internal class Client
     {
+        int id;
+        SeatManager seatManager;
+        bool lucky = false;
+
+        public Client(SeatManager seatManager, int id)
+        {
+            this.id = id;
+            this.seatManager = seatManager;
+        }
+
+        public void Run()
+        {
+            while (seatManager.seatIsAvailable() && !lucky)
+                lucky = seatManager.bookFirstAvailable(this);
+        }
     }
 }
--- SeatManager.cs
+++ SeatManager.cs
@@ -8,5 +8,14 @@ namespace Assignment2_Part2
 {
     internal class SeatManager
     {
+        List<Seat> seats;
+
+        public SeatManager(int numOfSeats)
+        {
+            seats = new List<Seat>();
+
+            for (int i = 0; i < numOfSeats; i++)
+                seats.Add(new Seat());
+        }
     }
 }
