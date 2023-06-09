7adc8b3024bcf5152a1d8e47c3617dd55a8c1c41
antn <antnl908@gmail.com>
2023-06-09 16:40:10 +0200

    Main
    main

--- Program.cs
+++ Program.cs
@@ -1,5 +1,33 @@
-﻿static void Main(string[] args)
-{
-    
-}
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Assignment2_Part2;
 
+class Assignment2P2
+{
+    // See https://aka.ms/new-console-template for more information
+
+    static void Main(string[] args)
+    {
+        bool isRunning = true;
+
+        int numOfClients = 20;
+        int numOfSeats = 35;
+        SeatManager seatManager = new SeatManager(numOfSeats);
+        Thread[] threads = new Thread[numOfClients];
+
+        Console.WriteLine("Hello, World!");
+
+        for (int i = 0; i < threads.Length; i++)
+        {
+            var c = new Client(seatManager, i);
+            threads[i] = new Thread(new ThreadStart(c.Run));
+            threads[i].IsBackground = true;
+            threads[i].Start();
+        }
+
+        while (isRunning)
+        {
+
+        }
+    }
+}
