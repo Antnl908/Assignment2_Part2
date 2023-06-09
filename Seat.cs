56d62f02236fd619f3f8975d009f41cfb8ae3580
antn <antnl908@gmail.com>
2023-06-09 16:15:27 +0200

    Updated Seat
    Updated seat

--- Seat.cs
+++ Seat.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,30 @@ namespace Assignment2_Part2
 {
     public class Seat
     {
-        class Status
+        int id;
+        int clientId;
+        public enum Status { available, occupied }
+        public Status status = Status.available;
+
+        public Seat(int id)
         {
-            public bool available;
+            this.id = id;
         }
 
-        Status status;
-
-        public Seat()
+        public void Book(int clientId)
         {
-            status = new Status();
+            this.clientId = clientId;
+            CurrentStatus = Status.occupied;
         }
+
+        public Status CurrentStatus
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+        public int Id => id;
+        public int ClientId { get => clientId; set => clientId = value; }
+        public bool IsAvailable { get => status == Status.available; }
     }
 }
