7f9285e23e2a20768dda7382d2e8797d5c8d3442
antn <antnl908@gmail.com>
2023-06-09 15:26:33 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+class Assignment2_Part2
+{
+    // See https://aka.ms/new-console-template for more information
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+    }
+}
+
