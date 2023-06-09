76200cf13da4cf1432aaf74ce07e22aad234ef83
antn <antnl908@gmail.com>
2023-06-09 15:34:53 +0200

    Initialize
    starting

---
+++ Seat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_Part2
+{
+    public class Seat
+    {
+        class Status
+        {
+            public bool available;
+        }
+
+        Status status;
+
+        public Seat()
+        {
+            status = new Status();
+        }
+    }
+}
