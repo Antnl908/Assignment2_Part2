c926fd56ac675e12c0a1db2db7963684f256a345
antn <antnl908@gmail.com>
2023-06-09 16:34:11 +0200

    Updated Main
    Updated Main

--- Program.cs
+++ Program.cs
@@ -1,11 +1,36 @@
-﻿
-class Assignment2_Part2
+﻿using System.Threading;
+using System.Threading.Tasks;
+namespace Assignment2_Part2
 {
-    // See https://aka.ms/new-console-template for more information
-    static void Main(string[] args)
+    class Assignment2P2
     {
-        Console.WriteLine("Hello, World!");
+        // See https://aka.ms/new-console-template for more information
 
+        static void Main(string[] args)
+        {
+            bool isRunning = true;
+
+            int numOfClients = 20;
+            int numOfSeats = 35;
+            SeatManager seatManager = new SeatManager(numOfSeats);
+            Thread[] threads = new Thread[numOfClients];
+
+            Console.WriteLine("Hello, World!");
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                var c = new Client(seatManager, i);
+                threads[i] = new Thread(new ThreadStart(c.Run));
+                threads[i].IsBackground = true;
+                threads[i].Start();
+            }
+
+            while(isRunning)
+            {
+
+            }
+        }
     }
+    
 }
 
